7e747cf6ab2448600b8626420d639820b0f5215b
Ken <ken@xpire0215.org>
2020-06-24 12:17:05 +0000

    Temp files

--- BoundryCondition.c
+++ BoundryCondition.c
@@ -1,34 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-#define _matT		3
-#define _dx			0.001
-#define _Imp 		pow(10,18)	
-#define _Inter 		pow(10,15)
+#define _matT		1
+#define _dx			0.1
+#define _Imp 		1e18
+#define _Inter 		1e15
 
-int main() {	
+int main() {
 	double matT 	= _matT;
 	double dx  		= _dx;
 	double Imp 		= _Imp;
 	double Inter	= _Inter;
-	int	   arraySize = matT / dx;
-	int	   mid 		= arraySize / 2;
+	int arraySize	= matT/dx;
+	int	mid 		= arraySize / 2;
+	int col			= arraySize;
+	int row			= arraySize;
 	double BC[arraySize];
 	
-	for (int i=0; i<mid; i++) {
+	for (int i = 0; i < mid; i++) {
 		BC[i] = Imp;
 		//printf("%.2f\n", BC[i]);
 	}
 
-	for	(int i=mid; i < arraySize; i++){
+	for	(int i = mid; i < arraySize; i++){
 		BC[i] =Inter;
-        //printf("%.2f\n" , BC[i] );
+        //printf("%.2f\n" , BC[i]);
 	}
 
-   // printf("%.2f\n%.2f\n", BC[mid-1], BC[mid]);
+    // printf("%.2f\n%.2f\n", BC[mid-1], BC[mid]);
 	
+    double dt=1;
+	double k=8.62e-5;
+	double T=1373;
+	double Dix0=2.22e8;
+	double Dixe=3.46;
+	double Dip0=4.23e9;
+	double Dipe=3.46;
+    double Dix=Dix0*exp(-1*Dixe/(k*T));
+    double Dip=Dip0*exp(-1*Dipe/(k*T));
+    double m0=9.11e-31;
+    double mn=1.08;
+    double mp=0.56;
+    double Eg=1.12;
+    double h=4.135e-15;
+    double PI=3.14159;
+	double ni=2*(pow(((2*PI*k*T)/(h*h)),1.5))*(pow((mn*mp*m0),0.75))*(exp((-1*Eg)/(2*k*T)));
+	double D=1.45e-3;
+	
+	double new_col[row];
+	for (int i = 0; i < row; i++) {
+		if (i == 0)
+			new_col[i] = Imp;
+		else if (i == row-1)
+			new_col[i] = Inter;
+		else
+			new_col[i] = (-1*BC[i-1]) + (((2*dx*dx)/(D*dt)+2)*BC[i]) - BC[i+1];
+	}
+	for (int i = 0; i < row-2; i++)
+		printf("%f\n", BC[i]);
 
+	printf("========\n");
 
+	for (int i = 0; i < row; i++)
+		printf("%f\n", new_col[i]);
+
+	// - BC[0] + (((2*dx*dx)/(D*dt)+2)*BC[1]) - BC[2] 
 	return 0;
 }
-		
+
---
+++ test.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MATERIAL	1
+#define DELTA_X		0.1
+#define IMP 		1e18
+#define INTER 		1e15
+
+//void printMatrix(double* matrix[row], int col, int row);
+void divideFirst(double** gaussianMatrix );
+double getLastNonZero(double* array);
+void eliminateUp2Down(double** gaussianMarix, int matSize);
+void eliminateDown2Up(double** gaussianMarix, int matSize);
+
+double* gaussianElimination(double* concentrationArray, int matSize);
+
+int main() {
+	double mat 		= MATERIAL;
+	double dx  		= DELTA_X;
+	double Imp 		= IMP;
+	double Inter	= INTER;
+
+	double dt=1;
+	double k=8.62e-5;
+	double T=1373;
+	double Dix0=2.22e8;
+	double Dixe=3.46;
+	double Dip0=4.23e9;
+	double Dipe=3.46;
+	double Dix=Dix0*exp(-1*Dixe/(k*T));
+	double Dip=Dip0*exp(-1*Dipe/(k*T));
+	double m0=9.11e-31;
+	double mn=1.08;
+	double mp=0.56;
+	double Eg=1.12;
+	double h=4.135e-15;
+	double PI=3.14159;
+	double ni=2*(pow(((2*PI*k*T)/(h*h)),1.5))*(pow((mn*mp*m0),0.75))*(exp((-1*Eg)/(2*k*T)));
+	double D=1.45e-3;
+	int time = 2;
+	int matSize	= mat/dx;
+	
+	// Alloc memories to matrix
+	double **matrix = (double **)malloc(time * sizeof(double *));
+	for (int i = 0; i < time; i++)
+		matrix[i] = (double *)malloc(matSize * sizeof(double));
+
+	// Init first concentration
+	for(int i = 0; i < matSize; i++) {
+		if(i < (matSize/2))
+			matrix[0][i] = Imp;
+		else
+			matrix[0][i] = Inter;
+	}
+	
+	for(int i = 1; i < time; i++) {
+		// Calculate new concentation of material
+		for(int j = 0; j < matSize; j++) {
+			if (j == 0)
+				matrix[i][j] = Imp;
+			else if (j == (matSize-1))
+				matrix[i][j] = Inter;
+			else
+				matrix[i][j] = (matrix[i-1][j-1]*(-1)) + (((2*dx*dx)/(D*dt)+2)*matrix[i-1][j]-matrix[i-1][j+1]);
+		}
+
+		// Gaussian elimination
+		gaussianElimination(matrix[0], matSize);
+	}
+
+	// Print all concentrations
+	for(int i = 0; i < time; i++) {
+		for(int j = 0; j < matSize; j++) 
+			printf("%f\n", matrix[i][j]);
+		printf("=====================\n");
+	}
+	return 0;
+}
+
+
+void divideFirst(double* array, int arraySize) {
+	double first = 0;
+	for (int i = 0; i < arraySize; i++) {
+		if (array[i] != 0) {
+			first = array[i];
+			break;
+		}
+	}
+
+	for (int i = 0; i < arraySize; i++) {
+		array[i] /= first;
+	}
+}
+
+void multiLast(double* array, double* multiArray int arraySize) {
+	double last = 0;
+	for (int i = (arraySize-1); i >= 0; i--) {
+		if (array[i] != 0) {
+			last = array[i];
+		}
+	}
+
+	for (int i = 0; i < arraySize; i++) {
+		multiArray[i] *= last;
+	}
+}
+
+void eliminateUp2Down(double** guassianMarix, int matSize, int pos) {
+	if(pos > 0) {
+		eliminateUp2Down(matrix, pos-1);
+		if (pos == (matSize-1)) {
+			divideFirst(matrix[pos-1]);
+		} else {
+			divideFirst(matrix[pos-1]);
+			for (int i = 0; i < matSize; i++) {
+				matrix[pos][i] -= matrix[pos-1][i];
+			}
+		}
+	}
+}
+
+void eliminateDown2Up(double** gaussianMatrix, int matSize, int pos) {
+	if(pos > 0) {
+		gaussianMatrix[pos] = multiLast(gaussianMatrix[pos], gaussianMatrix[pos+1]);
+		for (int i = 0; i < matSize; i++) {
+			matrix[pos][i] -= matrix[pos+1][i];
+		}
+		eliminateDown2Up(matrix, col-1);
+	}
+}
+
+
+double* gaussianElimination(double* concentrationArray, int matSize) {
+	double **gaussianMatrix = (double **)malloc(matSize * sizeof(double *));
+	for(int i = 0; i < matSize; i++)
+		gaussianMatrix[i] = (double *)calloc(matSize, sizeof(double));
+
+	for(int i = 0; i < matSize; i++) {
+		if(i == 0) {
+			gaussianMatrix[0][0] = 1;
+		} else if (i == (matSize-1)) {
+			gaussianMatrix[matSize-1][matSize-1] = 1;
+		} else {
+			gaussianMatrix[i][i-1] = 1;
+			gaussianMatrix[i][i] = 5;
+			gaussianMatrix[i][i+1] = 1;
+		}
+	}
+
+	for(int i = 0; i < matSize; i++) {
+		for(int j = 0; j < matSize; j++)
+			printf("%f, ", gaussianMatrix[i][j]);
+		printf("\n");
+	}
+
+	return concentrationArray;
+}
